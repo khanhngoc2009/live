34a6f47d5a701ce7f769b3e72855690bc316ec40
khanhngoc2009 <khanhpvph10443@fpt.ed.vn>
2021-07-06 09:52:17 +0700

    update live 06/07/2021 9:51AM

--- src/app/Screen/LiveScreen/layout/LiveRoomLayout.tsx
+++ src/app/Screen/LiveScreen/layout/LiveRoomLayout.tsx
@@ -1,6 +1,7 @@
 
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import {
+  Alert,
   Platform,
   SafeAreaView,
   ScrollView,
@@ -27,6 +28,7 @@ import { Live } from '@model/index'
 import { GenericListInitialState } from '@app/Screen/GetsentryScreen/slice/GenericListInitialState';
 import axios from 'axios'
 import { ApiURL } from '@app/network/api';
+import { flatMap } from 'lodash';
 interface Props { }
 
 interface State {
@@ -41,143 +43,119 @@ interface State {
   myUsername: string;
   rooms: { [name: string]: number };
   data?: Live[];
-  role?:boolean
-}
-class LiveRoomLayout extends Component<{ navigation, data, requestLiveThunk }, State> {
+  role?: boolean;
   _rtcEngine?: RtcEngine;
   _rtmEngine?: RtmEngine;
-  constructor(props?: State & any) {
-    super(props);
-    this.state = {
-      appId: '9f7cb444e3c84a788609a98dd123e75a',
-      token: null,
-      channelName: "",
-      inCall: false,
-      input: '',
-      inLobby: false,
-      peerIds: [],
-      seniors: [],
-      myUsername: '' + new Date().getTime(),
-      rooms: {},
-      data: [],
-      role:false
-    };
+}
+const LiveRoomLayout = ({ navigation }, props: State) => {
+  let { _rtcEngine, _rtmEngine } = props
+  const appDispatch = useAppDispatch()
+  const myLive = useAppSelector(state => state.LiveRoomReducer)
+  const myPro = useAppSelector(state => state.loginUserReducer)
+  const [appId, setAppId] = useState("9f7cb444e3c84a788609a98dd123e75a");
+  const [token, setToken] = useState(null)
+  const [channelName, setChannelName] = useState("")
+  const [inCall, setInCall] = useState(false)
+  const [input, setInput] = useState(myPro.userLogin?.email??"")
+  const [inLobby, setInLobby] = useState(false)
+  const [peerIds, setPeerIds] = useState([])
+  const [seniors, setSeniors] = useState([])
+  const [myUsername, setMyUsername] = useState("" + new Date().getTime())
+  const [rooms, setRooms] = useState({})
+  const [data, setData] = useState([])
+  const [role, setRole] = useState(true)
+
+  const checkFlatForm = () => {
     if (Platform.OS === 'android') {
       requestCameraAndAudioPermission().then(() => {
         console.log('requested!');
       });
     }
   }
-  getListMyProduct = () => {
-    this.props.requestLiveThunk();
+  
+  const getListMyProduct = () => {
+    appDispatch(requestLiveThunk())
   }
-  _request = async () => {
-    await axios.get(ApiURL.Live).then((res) => {
-      if (res.status === 200) {
-        this.setState({ data: res.data })
-      }
-    }).catch((e) => {
+  useEffect(() => {
+    initRTC();
+    initRTM();
+  })
 
+  useEffect(() => {
+    checkFlatForm()
+    // getListMyProduct()
+  }, [])
+
+  useEffect(() => {
+    return (() => {
+      _rtmEngine?.destroyClient();
+      _rtcEngine?.destroy();
     })
-  }
+  }, [])
 
-  componentDidMount() {
-    this.initRTC();
-    this.initRTM();
-    this._request();
-  }
-
-  _onCreate = (value?: Live) => {
+  const _onCreate = (value?: Live) => {
 
   }
-  componentWillUnmount() {
-    this._rtmEngine?.destroyClient();
-    this._rtcEngine?.destroy();
-  }
 
-  initRTC = async () => {
-    const { appId ,role} = this.state;
-    this._rtcEngine = await RtcEngine.create(appId);
-    await this._rtcEngine.enableVideo();
+  const initRTC = async () => {
+    _rtcEngine = await RtcEngine.create(appId);
+    await _rtcEngine.enableVideo();
 
-    this._rtcEngine.addListener('Error', (err) => {
+    _rtcEngine.addListener('Error', (err) => {
       console.log('Error', err);
     });
-    await this._rtcEngine.setChannelProfile(ChannelProfile.LiveBroadcasting)
-    await this._rtcEngine.setDefaultAudioRoutetoSpeakerphone(true);
-    await this._rtcEngine.setClientRole(!role ? ClientRole.Audience : ClientRole.Broadcaster)
-    this._rtcEngine.addListener('UserJoined', (uid) => {
-      // Get current peer IDs
-      const { peerIds, inCall, seniors, channelName } = this.state;
-      // If new user
+    // await _rtcEngine.setChannelProfile(ChannelProfile.LiveBroadcasting)
+    // await _rtcEngine.setDefaultAudioRoutetoSpeakerphone(true);
+    // await _rtcEngine.setClientRole(!role ? ClientRole.Audience : ClientRole.Broadcaster)
+    _rtcEngine.addListener('UserJoined', (uid) => {
       if (peerIds.indexOf(uid) === -1) {
         if (inCall && seniors.length < 2) {
-          this._rtmEngine?.sendMessageByChannelId(
+          _rtmEngine?.sendMessageByChannelId(
             'lobby',
             channelName + ':' + (peerIds.length + 2),
           );
         }
-        this.setState({
-          // Add peer ID to state array
-          peerIds: [...peerIds, uid],
-        });
+        setPeerIds([...peerIds, uid]);
       }
     });
-    
 
-    this._rtcEngine.addListener('UserOffline', (uid) => {
-      const { peerIds } = this.state;
-      this.setState({
-        // Remove peer ID from state array
-        peerIds: peerIds.filter((id) => id !== uid),
-      });
+
+    _rtcEngine.addListener('UserOffline', (uid) => {
+      setPeerIds(peerIds.filter((id) => id !== uid))
+
     });
-
-    // If Local user joins RTC channel
-    this._rtcEngine.addListener(
+    _rtcEngine.addListener(
       'JoinChannelSuccess',
       (channel, uid, elapsed) => {
         console.log('JoinChannelSuccess', channel, uid, elapsed);
-        this.setState({
-          inCall: true,
-        });
+        setInCall(true);
       },
     );
-    
   };
 
-  /**
-   * @name initRTM
-   * @description Function to initialize the Rtm Engine, attach event listeners and use them to sync usernames
-   */
-  initRTM = async () => {
-    let { appId, myUsername } = this.state;
-    this._rtmEngine = new RtmEngine();
+  const initRTM = async () => {
+    _rtmEngine = new RtmEngine();
 
-    this._rtmEngine.on('error', (evt) => {
+    _rtmEngine.on('error', (evt) => {
       console.log(evt);
     });
 
-    this._rtmEngine.on('channelMessageReceived', (evt) => {
-      // received message is of the form - channel:membercount, add it to the state
+    _rtmEngine.on('channelMessageReceived', (evt) => {
       let { text } = evt;
       let data = text.split(':');
-      this.setState({ rooms: { ...this.state.rooms, [data[0]]: data[1] } });
+      setRooms({ ...rooms, [data[0]]: data[1] });
     });
 
-    this._rtmEngine.on('messageReceived', (evt) => {
-      // received message is of the form - channel:membercount, add it to the state
+    _rtmEngine.on('messageReceived', (evt) => {
       let { text } = evt;
       let data = text.split(':');
-      this.setState({ rooms: { ...this.state.rooms, [data[0]]: data[1] } });
+      setRooms({ ...rooms, [data[0]]: data[1] });
     });
 
-    this._rtmEngine.on('channelMemberJoined', (evt) => {
-      let { channelName, seniors, peerIds, inCall } = this.state;
+    _rtmEngine.on('channelMemberJoined', (evt) => {
       let { channelId, uid } = evt;
-      // if we're in call and receive a lobby message and also we're the senior member (oldest member in the channel), signal channel status to joined peer
       if (inCall && channelId === 'lobby' && seniors.length < 2) {
-        this._rtmEngine
+        _rtmEngine
           ?.sendMessageToPeer({
             peerId: uid,
             text: channelName + ':' + (peerIds.length + 1),
@@ -187,18 +165,13 @@ class LiveRoomLayout extends Component<{ navigation, data, requestLiveThunk }, S
       }
     });
 
-    this._rtmEngine.on('channelMemberLeft', (evt) => {
+    _rtmEngine.on('channelMemberLeft', (evt) => {
       let { channelId, uid } = evt;
-      let { channelName, seniors, inCall, peerIds, rooms } = this.state;
       if (channelName === channelId) {
-        // Remove seniors UID from state array
-        this.setState({
-          seniors: seniors.filter((id) => id !== uid),
-          rooms: { ...rooms, [channelName]: peerIds.length },
-        });
+        setSeniors(seniors.filter((id) => id !== uid));
+        setRooms({ ...rooms, [channelName]: peerIds.length });
         if (inCall && seniors.length < 2) {
-          // if we're in call and we're the senior member (oldest member in the channel), signal channel status to all users
-          this._rtmEngine
+          _rtmEngine
             ?.sendMessageByChannelId(
               'lobby',
               channelName + ':' + (peerIds.length + 1),
@@ -208,93 +181,56 @@ class LiveRoomLayout extends Component<{ navigation, data, requestLiveThunk }, S
       }
     });
 
-    await this._rtmEngine.createClient(appId).catch((e) => console.log(e));
-    await this._rtmEngine
+    await _rtmEngine.createClient(appId).catch((e) => console.log(e));
+    await _rtmEngine
       ?.login({ uid: myUsername })
       .catch((e) => console.log(e));
 
-    await this._rtmEngine?.joinChannel('lobby').catch((e) => console.log(e));
-    this.setState({ inLobby: true });
+    await _rtmEngine?.joinChannel('lobby').catch((e) => console.log(e));
+    if (!inCall) {
+      setInLobby(true);
+    } else setInLobby(false)
   };
 
-  /**
-   * @name joinCall
-   * @description Function to join a room and start the call
-   */
-  joinCall = async (channelName: string) => {
-    
-    this.setState({ channelName:channelName});
-    
-    let { token } = this.state;
-    // Join RTC Channel using null token and channel name
-    await this._rtcEngine?.joinChannel(token, channelName, null, 0);
-    await this._rtmEngine?.joinChannel(channelName)
+  const joinCall = async (channelName: string) => {
+    setChannelName(channelName)
+    // setState({ channelName:channelName});
+
+    await _rtcEngine?.joinChannel(token, channelName, null, 0);
+    await _rtmEngine?.joinChannel(channelName)
       .catch((e) => console.log(e));
-    let { members } = await this._rtmEngine?.getChannelMembersBychannelId(
+    let { members } = await _rtmEngine?.getChannelMembersBychannelId(
       channelName,
     );
-    // if we're the only member, broadcast to room to all users on RTM
     if (members.length === 1) {
-      await this._rtmEngine
+      await _rtmEngine
         ?.sendMessageByChannelId('lobby', channelName + ':' + 1)
         .catch((e) => console.log(e));
     }
-    this.setState({
-      inLobby: false,
-      seniors: members.map((m: any) => m.uid),
-    });
+    setInLobby(false);
+    setSeniors(members.map((m: any) => m.uid))
   };
-
-  /**
-   * @name endCall
-   * @description Function to end the call and return to lobby
-   */
-  endCall = async () => {
-    let { channelName, myUsername, peerIds, seniors } = this.state;
-    // if we're the senior member, broadcast room to all users before leaving
-    if (seniors.length < 2) {
-      await this._rtmEngine
-        ?.sendMessageByChannelId('lobby', channelName + ':' + peerIds.length)
-        .catch((e) => console.log(e));
+  const endCall = async () => {
+    try {
+      if (seniors.length < 2) {
+        await _rtmEngine
+          ?.sendMessageByChannelId('lobby', channelName + ':' + peerIds.length)
+          .catch((e) => console.log(e));
+      }
+      Alert.alert("Message","đã thoát stream")
+      await _rtcEngine?.leaveChannel();
+      await _rtmEngine?.logout();
+      await _rtmEngine?.login({ uid: myUsername });
+      await _rtmEngine?.joinChannel('lobby');
+      setPeerIds([]);
+      setInCall(false);
+      setInLobby(true);
+      setSeniors([]);
+      setChannelName('');
+    } catch (error) {
     }
-    await this._rtcEngine?.leaveChannel();
-
-    await this._rtmEngine?.logout();
-    await this._rtmEngine?.login({ uid: myUsername });
-    await this._rtmEngine?.joinChannel('lobby');
-    this.setState({
-      peerIds: [],
-      inCall: false,
-      inLobby: true,
-      seniors: [],
-      channelName: '',
-    });
   };
-
-  render() {
-    const { inCall, channelName, inLobby ,role} = this.state;
-    return (
-      <SafeAreaView style={styles.max}>
-        <GoBackLayout title="Live Room"
-          onClickIcon={() => {
-            this.props.navigation.goBack();
-          }} />
-        <View style={styles.spacer}>
-          <Text style={styles.roleText}>
-            {inCall ? "You're in " + channelName : 'Lobby: Join/Create a room'}
-          </Text>
-        </View>
-        {this._renderRooms()}
-        {this._renderCall()}
-        {!inLobby && !inCall ? (
-          <Text style={styles.waitText}>Please wait, joining room...</Text>
-        ) : null}
-      </SafeAreaView>
-    );
-  }
-
-  _renderRooms = () => {
-    const { inLobby, rooms, input } = this.state;
+  const _renderRooms = () => {
     return inLobby ? (
       <View style={styles.fullView}>
         <Text style={styles.subHeading}>Room List</Text>
@@ -304,7 +240,7 @@ class LiveRoomLayout extends Component<{ navigation, data, requestLiveThunk }, S
               return (
                 <TouchableOpacity
                   key={index}
-                  onPress={() => this.joinCall(key)}
+                  onPress={() => joinCall(key)}
                   style={styles.roomsBtn}>
                   <Text>
                     <Text style={styles.roomHead}>{key}</Text>
@@ -325,16 +261,13 @@ class LiveRoomLayout extends Component<{ navigation, data, requestLiveThunk }, S
         </ScrollView>
         <TextInput
           value={input}
-          onChangeText={(val) => this.setState({ input: val })}
+          onChangeText={(val) => setInput(val)}
           style={styles.input}
-          placeholder="Enter Room Name"
+          placeholder={myPro.userLogin?.name}
         />
         <TouchableOpacity
           onPress={async () => {
-            this.setState({
-              role:true
-            })
-            input ? await this.joinCall(input) : null;
+            input? await joinCall(input):null;
           }}
           style={styles.button}>
           <Text style={styles.buttonText}>Create Room</Text>
@@ -343,8 +276,7 @@ class LiveRoomLayout extends Component<{ navigation, data, requestLiveThunk }, S
     ) : null;
   };
 
-  _renderCall = () => {
-    const { inCall, peerIds, channelName } = this.state;
+  const _renderCall = () => {
     return inCall ? (
       <View style={styles.fullView}>
         <RtcLocalView.SurfaceView
@@ -365,20 +297,36 @@ class LiveRoomLayout extends Component<{ navigation, data, requestLiveThunk }, S
             );
           })}
         </ScrollView>
-        <TouchableOpacity onPress={this.endCall} style={styles.button}>
+        <TouchableOpacity onPress={endCall} style={styles.button}>
           <Text style={styles.buttonText}>Leave Room</Text>
         </TouchableOpacity>
       </View>
     ) : null;
   };
-}
 
+  return (
+    <SafeAreaView style={styles.max}>
+      <GoBackLayout title="Live Room"
+        onClickIcon={() => {
+          navigation.goBack();
+        }} />
+      <View style={styles.spacer}>
+        <Text style={styles.roleText}>
+          {inCall ? "You're in " + channelName : 'Lobby: Join/Create a room'}
+        </Text>
+      </View>
+      {_renderRooms()}
+      {_renderCall()}
+      {!inLobby && !inCall ? (
+        <Text style={styles.waitText}>Please wait, joining room...</Text>
+      ) : null}
+    </SafeAreaView>
+  );
+}
 const mapStateToProps = (state: GenericListInitialState<Live>) => ({
   data: state.data
 });
-
 const mapDispatchToProps = {
   requestLiveThunk
 };
-
 export default connect(mapStateToProps, mapDispatchToProps)(LiveRoomLayout);
